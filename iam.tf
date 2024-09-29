71eb0b8c5abf6da8a19b5cc9e83299caa9479ebb
Vladislav <120631119+VladosusPing@users.noreply.github.com>
2024-09-29 21:46:06 +0400

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -6,23 +6,23 @@ resource "aws_iam_role" "github_actions_role" {
   name = "github-actions-role"
 
   assume_role_policy = jsonencode({
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "arn:aws:iam::692859951121:oidc-provider/token.actions.githubusercontent.com"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
-          "token.actions.githubusercontent.com:sub": "repo:VladosusPing/rsschool-devops-course-tasks:ref:refs/heads/main"
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::692859951121:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:VladosusPing/rsschool-devops-course-tasks:ref:refs/heads/main"
+          }
         }
       }
-    }
-  ]
-})
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "attach_ec2_full_access" {
