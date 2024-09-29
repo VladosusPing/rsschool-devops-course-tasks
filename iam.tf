5667945b1c6ada52a466e7705929d64c8e58cd12
Vladislav <120631119+VladosusPing@users.noreply.github.com>
2024-09-29 21:33:39 +0400

    Update iam.tf
    updated format

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1" # Укажите ваш регион
+  region = "us-east-1"
 }
 
 resource "aws_iam_role" "github_actions_role" {
@@ -36,26 +36,27 @@ resource "aws_iam_role_policy_attachment" "attach_route53_full_access" {
   role       = aws_iam_role.github_actions_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
 }
+
 resource "aws_iam_role_policy_attachment" "attach_s3_full_access" {
   role       = aws_iam_role.github_actions_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
-            
+
 resource "aws_iam_role_policy_attachment" "attach_iam_full_access" {
   role       = aws_iam_role.github_actions_role.name
   policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
 }
-    
+
 resource "aws_iam_role_policy_attachment" "attach_vpc_full_access" {
   role       = aws_iam_role.github_actions_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
 }
-  
+
 resource "aws_iam_role_policy_attachment" "attach_sqs_full_access" {
   role       = aws_iam_role.github_actions_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
 }
-  
+
 resource "aws_iam_role_policy_attachment" "attach_eventbridge_full_access" {
   role       = aws_iam_role.github_actions_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
