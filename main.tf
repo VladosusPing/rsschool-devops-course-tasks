2f27d576a38ab6df9bbbd27f555c8392cf840834
Vladislav <120631119+VladosusPing@users.noreply.github.com>
2024-09-29 19:45:05 +0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "components-terraform-state"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "components-terraform-lockid"
+  }
+}
