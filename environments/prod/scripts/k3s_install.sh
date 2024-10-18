a4534ee335370b43703398d2a8db44703f6e39ff
VladosusPing <vlad.ap15@gmail.com>
2024-10-18 11:46:37 +0400

    Added INSTALL_K3S_CHANNEL var to k3s installation script Added installation complete message

--- environments/prod/scripts/k3s_install.sh
+++ environments/prod/scripts/k3s_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 # Update the package database and install necessary dependencies
 apt-get update
 apt-get install -y curl
@@ -8,8 +10,13 @@ apt-get install -y curl
 swapoff -a
 
 # Install K3s using the official K3s install script
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=latest sh -
+
+# Optional: Enable K3s to start on boot
+systemctl enable k3s
 
 # Cleanup
-apt-get autoremove
+apt-get autoremove -y
 apt-get clean
+
+echo "K3s all-in-one cluster installation complete. You can verify with 'kubectl get nodes'."
